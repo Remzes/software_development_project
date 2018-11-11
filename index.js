3ff002846de14760d042353aade136235dae6604
suntekOh <suntaeck@naver.com>
2018-11-10 19:57:32 -0500

    Story #160777722: Implement UI screen for home page with 'Login' button - added mongoose connection

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
