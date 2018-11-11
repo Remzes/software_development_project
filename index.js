a6cf651561787ce0780bdf3e30d3a549edaa1325
suntekOh <suntaeck@naver.com>
2018-11-10 19:56:06 -0500

    Story #160777722: Implement UI screen for home page with 'Login' button - set up index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const expressStaticGzip = require("express-static-gzip");
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
