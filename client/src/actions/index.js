cc97afe161cf63674915dd4e4e1404f64605ac58
Remzes <victor_s1998@mail.ru>
2018-11-10 21:20:32 -0500

    #160778866: Integration of UI, server-side part, and database to manipulate survey informations

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,3 +7,34 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   dispatch({type: FETCH_USER, payload: res.data});
 };
+
+//AC to interact with Stripe API - adding credits
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe/', token);
+  dispatch({type: FETCH_USER, payload: res.data});
+};
+
+//Submit survey to the system
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({type: FETCH_USER, payload: res.data});
+};
+
+//Get surveys from the system
+export const fetchSurveys = () => dispatch => {
+  const request = axios.get('/api/surveys');
+  dispatch({type: FETCH_SURVEYS, payload: request.then(result => result.data)})
+};
+
+
+//Load slider configs to the system
+export const loadConfigs = () => dispatch => {
+  dispatch({type: FETCH_CONFIG, payload: loadAllImages()});
+};
+
+//Select config for the slider
+export const selectConfig = (id) => dispatch => {
+  dispatch({type: SELECT_CONFIG, payload: id})
+};
