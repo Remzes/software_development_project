5d4c24586d9c47ce59b6404c6134530faf2a9eb8
suntekOh <suntaeck@naver.com>
2018-11-10 19:59:45 -0500

    Story #160777722: Implement UI screen for home page with 'Login' button - index.js good to go

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ const keys = require('./config/keys');
 require('./models/User');
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+//End of routes
+
+if (process.env.NODE_ENV === 'production') {
+  //Serve main.js and main.css files
+  app.use(express.static('client/build'));
+
+  //Serve index.html in case of unknown route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT);
